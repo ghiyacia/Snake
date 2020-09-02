1a83532e11b4215422a2bcc5d76e74f7731876a5
ghiyacia <ghilesyacia@gmail.com>
2020-09-02 20:15:58 +0200

    fixer l'erreur checkCollision

--- snake.js
+++ snake.js
@@ -1,83 +1,123 @@
-var ctx = 0 ;
-var canva = 0;
-var py = 0;var tc = 0;var ay = 0 ;var yv = 0;
-var px = py = 10;
-var gs = tc = 20;
-var ax = ay = 15;
-var xv = yv = 0;
+window.onload = function(){
+    var canva = document.getElementById("canvas");       
+    var ctx = canva.getContext("2d");
 
-var trail = [];
-var tail = 5;
+    var canvaW = canva.width;
+    var canvaH = canva.height;
+    var direction = "right";
+    var snakeW = 10;
+    var snakeH = 10;
 
-window.onload = function (){
-    //quan une touche et presser on enclanche l'evenement
-    canva = document.getElementById('canv');
-    ctx = canva.getContext("2d");
-    document.addEventListener('keydown',keyPush) 
-    setInterval(play,1000/15);
 
-}
-
-function play(){
-    px += xv;
-    py += yv;
+    // controlled snake event
+    document.addEventListener("keydown",getDirection);
     
-    if (px < 0){
-        px = tc - 1;
-    }
-
-    if (px > tc - 1){
-        px = 0;
-    }
-
-    if (py < 0){
-        py = tc -1;
-    }
-
-    if (py > tc - 1){
-        py = 0;
-    }
-    ctx.fillStyle= 'black';
-    ctx.fillRect(0,0,canva.width,canva.height)
-
-    ctx.fillStyle="lime";
-    for(var i = 0; i < trail.length;i++){
-        ctx.fillRect(trail[i].x * gs, trail[i].y * gs, gs - 2,gs - 2);
-        if (trail[i].x == px && trail[i].y == py){
-            tail = 5;
+    // controlled snake function
+    function getDirection(e){
+        if (e.keyCode == 37 && direction != "right"){
+            direction = "left";
+        }else if (e.keyCode == 38 && direction != "down") {
+            direction = "up";
+        }else if (e.keyCode == 39 && direction != "left"){
+            direction = "right";
+        }else if (e.keyCode == 40 && direction != "up"){
+            direction = "down";
         }
     }
+    // deaw snake
+    function drawSnake(x, y){
+    // size cel
+    //snake style 
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(x*snakeW, y*snakeH,snakeW, snakeH);
 
-    trail.push({x:px,y:py});
-    while (trail.length > tail){
-        trail.shift();
+    ctx.fillStyle = "#000";
+    ctx.strokeRect(x*snakeW, y*snakeH, snakeW, snakeH);
     }
 
-    if (ax == px && ay == py){
-        tail++;
-        ax = Math.floor(Math.random() * tc);
-        ay = Math.floor(Math.random() * tc);
 
+    var lenS = 4;
+    var snake = [];
+
+    for (let i = lenS -1; i >= 0 ; i--) {
+        snake.push(
+            {
+                x:i,
+                y:0
+            }
+        );
     }
-    ctx.fillStyle = "red";
-    ctx.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
-}
-
-function keyPush(evt){
-
-    switch(evt.keyCode){
-        case 37:
-            xv = -1; yv = 0;
-            break;
-        case 38:
-            xv = 0; yv = -1;
-            break;
-        case 39:
-            xv = 1; yv = 1;
-            break;
-        case 40:
-            xv = 0; yv = 1;
-            break;
+    //    creat food object
+    food = {
+        x: Math.round(Math.random()*(canvaW/snakeW-1)+1),
+        y: Math.round(Math.random()*(canvaH/snakeH-1)+1)
     }
-            
+
+    function drawfood(x, y){
+        // size food and style 
+        ctx.fillStyle = "red";
+        ctx.fillRect(x*snakeW, y*snakeH,snakeW, snakeH);
+        ctx.fillStyle = "red";
+        ctx.strokeRect(x*snakeW, y*snakeH, snakeW, snakeH);
+    }
+
+    function checkCollision(x, y, array){
+        for (let i = 0; i < array.length; i++) {
+            if (x == array[i].x && y == array[i].y) {
+                return true;
+            }           
+        }
+        return false;
+    }
+    function draw(){
+        ctx.clearRect(0, 0, canvaW, canvaH);
+        for (let i = 0; i < snake.length; i++) {
+            var x = snake[i].x;
+            var y = snake[i].y;
+            drawSnake(x, y);
+        }
+
+        drawfood(food.x, food.y);
+        
+        var snakeX = snake[0].x
+        var snakeY = snake[0].y
+        // console.log(checkCollision(snakeX, snakeY, snake))
+
+        // hits the wall
+        if(snakeX < 0 || snakeY < 0 || snakeX >= canvaW/snakeW || snakeY >= canvaH/snakeH || checkCollision(snakeX, snakeY, snake) != false){
+            // location.reload();
+            console.log ("sa fait mal")
+        }
+
+        //remove the snake tail
+        // snake.pop();
+
+        // creat the new head 
+        if (direction == 'left')snakeX--;
+        else if (direction == "up")snakeY--; 
+        else if (direction == "right")snakeX++;
+        else if (direction == "down")snakeY++
+
+        // eats the food
+        if (snakeX == food.x && snakeY == food.y){
+            food = {
+                x: Math.round(Math.random()*(canvaW/snakeW-1)+1),
+                y: Math.round(Math.random()*(canvaH/snakeH-1)+1)
+            }
+            var newHead = {
+                x:snakeX,
+                y:snakeY
+            };
+        }else{
+            snake.pop();
+            var newHead = {
+                x:snakeX,
+                y:snakeY
+            };
+        }
+        snake.unshift(newHead);
+    }
+    setInterval(draw,60)
+
+
 }
